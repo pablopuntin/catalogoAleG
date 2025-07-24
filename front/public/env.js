fbf986128f76fd4ef2d53134386de79ff835c8b2
pablo putin <pablopuntin@gmail.com>
2025-07-24 19:42:31 -0300

    modificaciones en vistas0

--- front/public/env.js
+++ front/public/env.js
@@ -1,5 +1,5 @@
 window.env = {
-  API_URL: //"https://catalogo-aleg-back.onrender.com" 
-   "http://localhost:3000"
+  API_URL: "https://catalogo-aleg-back.onrender.com" 
+   //"http://localhost:3000"
 };
 
